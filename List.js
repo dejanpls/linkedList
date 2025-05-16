3f3319f0e67fd974d47bff45180b40b1ce6788b2
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-05-16 21:49:41 +0200

    feat: implement .head and .tail methods to return head/tail of a list respectfully

--- List.js
+++ List.js
@@ -1,17 +1,17 @@
 import Node from "./Node.js";
 
 export default class List {
-  head = null;
+  #head = null;
 
   append(value) {
     const node = new Node(value);
 
-    if (this.head === null) {
-      this.head = node;
+    if (this.#head === null) {
+      this.#head = node;
       return;
     }
 
-    let current = this.head;
+    let current = this.#head;
 
     while (current.next !== null) current = current.next;
     current.next = node;
@@ -20,19 +20,33 @@ export default class List {
   prepend(value) {
     const node = new Node(value);
 
-    node.next = this.head;
-    this.head = node;
+    node.next = this.#head;
+    this.#head = node;
   }
 
-  size() {
-    if (this.head === null) return 0;
+  get size() {
+    if (this.#head === null) return 0;
 
     let size = 1;
-    let current = this.head;
+    let current = this.#head;
     while (current.next !== null) {
       current = current.next;
       size++;
     }
     return size;
+  }
+
+  get head() {
+    return this.#head;
+  }
+
+  get tail() {
+    if (this.#head === null) return this.#head;
+
+    let tail = this.#head;
+    while (tail.next !== null) {
+      tail = tail.next;
+    }
+    return tail;
   }
 }
